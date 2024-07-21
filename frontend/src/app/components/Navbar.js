57ee7aeb598ce647d8e1db8ca9c3b58c66e08022
dhrumilankola <dhrumilankola843@gmail.com>
2024-07-21 02:34:14 -0700

    hyperlink added to logo

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
     <nav className="bg-white text-gray-800 shadow-lg mb-10 rounded-lg mx-10">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold text-blue-600">Insurance Info</div>
+        <div className="text-2xl font-bold text-blue-600"><Link href="/" className="hover:text-blue-600">Insurance Info</Link>
+        </div>
         <div className="space-x-6">
           <Link href="/" className="hover:text-blue-600">Home</Link>
           <Link href="/contact" className="hover:text-blue-600">Contact Us</Link>
